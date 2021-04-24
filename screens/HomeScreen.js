422a674ccb4088927868e2fbf0fd21fcaad898fe
Deepthy <drudradas@gmail.com>
2021-04-25 02:06:44 +0530

    Style

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"blue",
+    backgroundColor:"coral",
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
