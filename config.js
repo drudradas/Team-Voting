e99b9bd2e789e3367f28dad451166f7a4b46b63e
Deepthy <drudradas@gmail.com>
2021-04-25 01:12:36 +0530

    Config Changed

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
 import firebase from 'firebase';
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyBpxcDgEguZ7oeqEEI-IS8PF6xKpwrY05E",
-    authDomain: "teamvotingapp.firebaseapp.com",
-    databaseURL: "https://teamvotingapp-default-rtdb.firebaseio.com",
-    projectId: "teamvotingapp",
-    storageBucket: "teamvotingapp.appspot.com",
-    messagingSenderId: "990395711535",
-    appId: "1:990395711535:web:e2dac63cea3c596561c6da"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyCwXq1FV8N151orYeGIKRSlA7P6Lhl347U",
+  authDomain: "team-voting-5fbb8.firebaseapp.com",
+  databaseURL: "https://team-voting-5fbb8-default-rtdb.firebaseio.com",
+  projectId: "team-voting-5fbb8",
+  storageBucket: "team-voting-5fbb8.appspot.com",
+  messagingSenderId: "876249878969",
+  appId: "1:876249878969:web:c73b1c42433274a160e668"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
   export default firebase.database();
