13cf7a40f741f41ed7ca44e2fdd13f0934e01f21
MarcusHoHZ <74183997+MarcusHoHZ@users.noreply.github.com>
2021-09-09 20:00:00 +0800

    Add files via upload

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -13,7 +13,7 @@ export default class AppHeader extends React.Component{
 
 const styles = StyleSheet.create({
   textContainer:{
-    backgroundColor: '#FFC300'
+    backgroundColor: '#083872'
   },
   text:{
     padding: 20,
