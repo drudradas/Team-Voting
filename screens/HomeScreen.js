a24d7492519540ecab8b4bd5c5c8e7ab0f2eaf4c
Shivanshp4s <79980567+Shivanshp4s@users.noreply.github.com>
2021-09-22 16:44:03 +0530

    Update HomeScreen.js
    Made a round button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -70,10 +70,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
-    borderRadius: 15,
+    borderRadius: 100,
     margin:10,
     width: 150,
-    height: 50,
+    height: 100,
   },
   ratingContainer: {
     alignSelf: 'center',
