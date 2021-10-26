4f8fdf90fca2f99d0f9034fb689162b9c4bfa0b0
Quiver <d3v.exgaming@gmail.com>
2021-10-26 19:40:03 -0400

    config update
    ee

--- config.js
+++ config.js
@@ -1,9 +1,20 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+// var firebaseConfig = {
 
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJpD2t_llXPE-wFz3ptxUutRAuwvJLwVM",
+  authDomain: "team-voting-71246.firebaseapp.com",
+  databaseURL: "https://team-voting-71246-default-rtdb.firebaseio.com",
+  projectId: "team-voting-71246",
+  storageBucket: "team-voting-71246.appspot.com",
+  messagingSenderId: "651167380918",
+  appId: "1:651167380918:web:f70d288c3b7e381b4b3d17"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
