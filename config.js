7c83b601f338318faca248a82cd0cca4eb0d20dd
nixmin23 <86486020+nixmin23@users.noreply.github.com>
2021-12-22 18:10:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
-};
+    apiKey: "AIzaSyA7kbulRbZVj0gBhDeIjo9AhO71Ky0rzJc",
+    authDomain: "team-voting-109ea.firebaseapp.com",
+    projectId: "team-voting-109ea",
+    storageBucket: "team-voting-109ea.appspot.com",
+    messagingSenderId: "527597580414",
+    appId: "1:527597580414:web:9d886b342d28973cbea82f"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
