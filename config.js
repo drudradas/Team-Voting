ffba4047c5f8d0b873ad73b108626357c4da2485
Chakram2008 <sudharsan912saravanan@gmail.com>
2022-03-28 11:44:06 +0530

    Added the firebase URL

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyD4sJ3APtjggFl7lDa-IvWa8ocg90awHG0",
+    authDomain: "project-58-d6eed.firebaseapp.com",
+    databaseURL: "https://project-58-d6eed-default-rtdb.firebaseio.com",
+    projectId: "project-58-d6eed",
+    storageBucket: "project-58-d6eed.appspot.com",
+    messagingSenderId: "267951069841",
+    appId: "1:267951069841:web:134e8e4f53d6d84bcafd30"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
