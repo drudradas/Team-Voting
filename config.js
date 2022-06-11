b2575d640756ceec6f878637ad5515f60e56cbc3
Ceramicbloon <79235916+Ceramicbloon@users.noreply.github.com>
2022-06-11 13:43:28 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,13 @@ import firebase from 'firebase';
 // add SDK Firebase
 var firebaseConfig = {
 
+    apiKey: "AIzaSyBmylE7dTzAL46Ql-4Fh9fqkdHBKbFsHrY",
+    authDomain: "aadsadsadsads-508e5.firebaseapp.com",
+    projectId: "aadsadsadsads-508e5",
+    storageBucket: "aadsadsadsads-508e5.appspot.com",
+    messagingSenderId: "278581976532",
+    appId: "1:278581976532:web:5ca425c4b534a909564a41"
+
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
