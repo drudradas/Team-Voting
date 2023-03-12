8a6067bf476a1074a2804fa0fca08d1a81e24693
Aditya03adi <73061574+Aditya03adi@users.noreply.github.com>
2023-03-12 10:18:53 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,13 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+    apiKey: "AIzaSyBJtzxsdwisuY_MmvatSKqEAAww2aRn4I4",
+  authDomain: "c-37-quiz-game.firebaseapp.com",
+  databaseURL: "https://c-37-quiz-game-default-rtdb.firebaseio.com",
+  projectId: "c-37-quiz-game",
+  storageBucket: "c-37-quiz-game.appspot.com",
+  messagingSenderId: "434701679368",
+  appId: "1:434701679368:web:1d2a247dd72e785b7acabd"
 
 };
 // Initialize Firebase
